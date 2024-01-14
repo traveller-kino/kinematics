0555fa88e5f8f756173affd05c0a6471c8aa1cda
Kino <84192243+traveller-kino@users.noreply.github.com>
2024-01-14 13:47:47 -0500

    implemented scan code timing, changed to CSV format, so analyzers can make their own decisions on # of dimensions

--- keyboard.py
+++ keyboard.py
@@ -1,33 +1,10 @@
-import sqlite3
 import pynput, time
 from time import perf_counter
 from pynput.keyboard import Key,Listener
-from pathlib import Path
-import shutil
 import csv
 
-# Check if there is already a profile, if not, copy the DB prototype
-dbFile = Path("./keyboard.db")
-if not dbFile.is_file():
-    shutil.copy2("./keyboard.db.prototype", "./keyboard.db")
-
-translationFile = open('./vkMappings.csv')
-translationDict = csv.DictReader(translationFile, fieldnames=['key', 'vkCode'], quotechar='"', delimiter=',', escapechar="\\")
-memDict = dict()
-counter = 0
-for row in translationDict:
-    memDict[0x20 + counter] = row['vkCode']
-    counter += 1
-nMemDict = dict()
-counter = 0
-for row in memDict: # Invert, because manipulating the CSV is damn near impossible
-    nMemDict[memDict[row]] = 0x20 + counter
-    counter += 1
-memDict = nMemDict
-
-translationFile.close()
-connection = sqlite3.connect('keyboard.db')
-cursor = connection.cursor()
+# from pathlib import Path
+# import shutil
 
 """ TODO: 
     * Need to keep track of the raw keystrokes (virtual key codes) AND the outcome (printed UTF-8)
@@ -35,57 +12,49 @@ cursor = connection.cursor()
     * Change the even/odd design to an explicit ring buffer
     * Dump outputs to a CSV, because an n^2 table is too impractical
 """
-class KeyCorrelator:
-    RIX = 0
-    def __init__(self):
-        self.evenTimer = 0
-        self.evenLastKey = 'NULL'
-        self.oddTimer = 0
-        self.oddLastKey = 'NULL'
+class Manager:
+    def __init__(self, csvFiles, csvWriters):
+        self.keytimes = [time.perf_counter()] # Note: Windows may reduce the precision to as low as 20ms based on various factors.
+        self.scancodes = [0]
+        self.utfoutput = [0]
+        self.csvFiles = csvFiles
+        self.csvWriters = csvWriters
 
     def press(self, key):
-        if KeyCorrelator.RIX % 2 == 0:
+        isFnOrMod = None
+        scanCode = None
+        try:
+            scanCode = key.vk
+            isFnOrMod = False
+        except AttributeError:
+            scanCode = key.value.vk
+            isFnOrMod = True
+        
+        currentPrecisionTime = time.perf_counter()
+        if ( currentPrecisionTime - (self.keytimes[-1]) ) > 2.5:
+            # Keystroke is stale. Associatively insert a "stub", so that analyzers know to dispose of as well.
+            self.keytimes.append(currentPrecisionTime)
+            self.scancodes.append(0)
+            self.utfoutput.append(0)
+            return
+        
+        self.keytimes.append(currentPrecisionTime)
+        self.scancodes.append(scanCode)
+        self.utfoutput.append(-1) # TODO: Implement functionality to determine what is actually being printed to the screen. e.g. if the previous key was shift, and this key is 'a', it should really be 'A', not 'a'.
 
-            try:
-                if key.vk < 0x00 or key.vk > 0xFFFF:
-                    return
-            except:
-                if not (key.value.vk < 0x00 or key.value.vk > 0xFFFF):
-                    key = chr(key.value.vk)
-                    pass
-                else: return
-            self.evenTimer = time.perf_counter()
-            self.evenLastKey = key
+        record = '"{}","{}","{}","{}"'.format(str(self.keytimes[-1]), str(self.keytimes[-1] - self.keytimes[-2]), str(self.scancodes[-2]), str(self.scancodes[-1]))
+        self.csvWriters[0].writerow([str(self.keytimes[-1]), str(self.keytimes[-1] - self.keytimes[-2]), str(self.scancodes[-2]), str(self.scancodes[-1])])
+        if len(self.scancodes) % 10 == 0: self.csvFiles[0].flush()
+        print(record)
 
-            diff = self.evenTimer - self.oddTimer
-            if diff > 2.5: 
-                KeyCorrelator.RIX += 1
-                return
-            print(str(self.oddLastKey) + "\t\t" + "=>" + "\t" + str(self.evenLastKey) + "\t\t" + str(diff))
-            KeyCorrelator.RIX += 1
-        else:
-            
-            try:
-                if key.vk < 0x00 or key.vk > 0xFFFF:
-                    return
-            except:
-                if not (key.value.vk < 0x00 or key.value.vk > 0xFFFF):
-                    key = chr(key.value.vk)
-                    pass
-                else: return
-            self.oddTimer = time.perf_counter()
-            self.oddLastKey = key
+scanCodeRecordHeaders = ["Timestamp", "Delta", "PreviousScanCode", "ScanCode"]
+scanCodeCsv = open('scanCodeRecord.csv', 'a', newline='')
+scanCodeWriter = csv.writer(scanCodeCsv, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL )
 
-            diff = self.oddTimer - self.evenTimer
-            if diff > 2.5: 
-                KeyCorrelator.RIX += 1
-                return
-            print(str(self.evenLastKey) + "\t\t" + "=>" + "\t" + str(self.oddLastKey) + "\t\t" + str(diff))
-            KeyCorrelator.RIX += 1
+print('"Timestamp","Delta","PrevScanCode","ScanCode"')
 
-timer = KeyCorrelator()
+timer = Manager([scanCodeCsv, None], [scanCodeWriter, None])
 with Listener(on_press=timer.press) as listener:
     listener.join()
 
-connection.commit()
-connection.close()
+scanCodeCsv.close()
